88f352d096e5a5e6b4997509c03e25b0b6db1225
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-15 20:29:48 -0300

    config

--- build/main.js
+++ build/main.js
@@ -6,6 +6,6 @@ require("dotenv/config");
 
 var _app = _interopRequireDefault(require("./config/app"));
 
-var APP_PORT = process.env.APP_PORT;
+var PORT = process.env.PORT;
 
-_app["default"].listen(APP_PORT);
+_app["default"].listen(PORT || 3000);
